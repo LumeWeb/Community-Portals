dc60951659f92e5083ea333cb57a6403f9c90a2e
Derrick Hammer <derrick@derrickhammer.com>
2023-06-29 01:15:27 -0400

    refactor: use new testing portal

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import { Portal } from "@lumeweb/libweb";
 
 const DEFAULT_PORTAL_LIST: Portal[] = [
-  { id: "lumeweb", url: "https://web3portal.com", name: "web3portal.com" },
+  {
+    id: "lumeweb",
+    url: "https://alpha.web3portal.com",
+    name: "web3portal.com",
+  },
 ];
 
 export default DEFAULT_PORTAL_LIST;
